045f5864fa935e738e5d62d42ec9eb082ef0d458
Nigar Adil <nigaradil@Nigars-MacBook-Air.local>
2023-09-24 05:40:42 +0400

    The html and css of the registration and login pages were prepared

---
+++ asset/js/registration.js
@@ -0,0 +1,16 @@
+{/* <script>
+const registrationForm = document.getElementById("registrationForm");
+const errorMessage = document.getElementById("errorMessage");
+
+registrationForm.addEventListener("submit", function (event) {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+        event.preventDefault();
+        errorMessage.textContent = "Şifreler eşleşmiyor.";
+    } else {
+        errorMessage.textContent = "";
+    }
+});
+</script> */}
